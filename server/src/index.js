4728fc7a1ba00afd9f0a67bfa75538b4c48bc093
HimAYouN <himayoun7931@gmail.com>
2026-02-17 09:37:28 +0530

    BE Folder structure

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+import app from './app.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
+
+dotenv.config()
+
+const port = process.env.PORT
+
+
+const startServer = async () => {
+  try {
+    await connectDB()
+    console.log("MongoDB Connected Successfully");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+
+  } catch (error) {
+    console.log("Error while starting the server: ", error);
+    process.exit(1)
+  }
+
+}
+
+startServer()
+
+
